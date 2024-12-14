d09315384b33edfec7ac59fcdf121bf712f35402
Diallo <mamadouadama609@gmail.com>
2024-12-13 20:49:03 -0600

    Second C

--- src/main/java/com/example/students/controller/StudentController.java
+++ src/main/java/com/example/students/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/students")
@@ -36,10 +37,11 @@ public class StudentController {
         return "redirect:/students";
     }
 
-    @GetMapping("/delete")
-    public String deleteStudent(@ModelAttribute Student student,  ) {
-        System.out.println(student);
-        studentRepository.delete(student);
+    @PostMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable Long id, Model model, RedirectAttributes redirectAttributes) {
+        //System.out.println(student);
+        studentRepository.deleteById(id);
+        redirectAttributes.addFlashAttribute("message", "Student has been deleted");
         return "redirect:/students";
     }
 }
